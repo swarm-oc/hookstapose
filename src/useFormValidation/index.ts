8c03c0003a008ec01b2c794b877f40e0d9b2f050
MaanuVazquez <maanuvazquez@gmail.com>
2019-03-25 18:24:11 -0300

    feat(useValidationForm): add resetFields and manuallySetValue

--- src/useFormValidation/index.ts
+++ src/useFormValidation/index.ts
@@ -1,143 +1,184 @@
 // @flow
-import {useReducer, SyntheticEvent} from 'react'
-
-const CHANGE_VALUE = 'changeValue'
-const VALIDATE_FIELD = 'validateField'
+import {useReducer} from 'react'
+import { CHANGE_VALUE, VALIDATE_FIELD, RESET_FIELDS } from './actions'
 
 interface IOptions {
-    form?: {
-        onSubmit?: (e: React.SyntheticEvent<HTMLFormElement>) => void | Promise<void>;
-    };
-    fields: {
-        [field: string]: {
-            value?: string;
-            rules: ((value: string) => void)[];
-        };
-    };
+  form?: {
+    onSubmit?: (e: React.SyntheticEvent<HTMLFormElement>) => void | Promise<unknown>,
+  },
+  fields: {
+    [field: string]: {
+      value?: string,
+      rules: ((value: string) => void)[],
+    },
+  },
 }
 
 interface IFormValidationResult {
-    form: {
-        noValidate: true;
-        onSubmit: (e: React.SyntheticEvent<HTMLFormElement>) => void;
-    };
-    fields: {
-        [field: string]: {
-            input: {
-                value: string;
-                onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-            };
-            info: {
-                status: 'standard' | 'success' | 'error';
-                message?:  string;
-            };
-        };
-    };
+  form: {
+    noValidate: true,
+    onSubmit: (e: React.SyntheticEvent<HTMLFormElement>) => void,
+  },
+  fields: InputProps,
+  resetFields: () => void,
+  manuallySetField: (fieldName: string, value: string) => void,
+}
+
+type InputProps = {
+  [field: string]: {
+    input: {
+      onChange: any,
+      value: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    },
+    info: Status,
+  },
 }
 
 type State = {
-    values: {
-        [field: string]: string;
-    };
-    statuses: {
-        [field: string]: {
-            status: 'standard' | 'success' | 'error';
-            message?: string;
-        };
-    };
+  values: {
+    [field: string]: string,
+  },
+  statuses: {
+    [field: string]: Status,
+  },
+}
+
+type Status = {
+  status: 'standard' | 'success' | 'error',
+  message?: string,
 }
 
 type Action = {
-    type: string;
-    field: string;
-    payload: any;
+  type: string,
+  field?: string,
+  payload?: any,
+}
+
+function resetAllFields(state: State): State {
+  return Object.keys(state.values).reduce<State>((acum: State, fieldName: string) => {
+    acum.values[fieldName] = ''
+    acum.statuses[fieldName] = {
+      status: "standard"
+    }
+    return acum
+  }, {
+    values: {}, statuses: {}
+  })
 }
 
 function reducer(state: State, {type, field, payload}: Action): State  {
-    switch (type) {
-        case CHANGE_VALUE:
-            return {...state, values: {...state.values, [field]: payload}}
-        case VALIDATE_FIELD:
-            return {...state, statuses: {...state.statuses, [field]: payload}}
-        default:
-            throw Error()
-    }
+  switch (type) {
+    case CHANGE_VALUE:
+      return {...state, values: {...state.values, [field!]: payload}}
+    case VALIDATE_FIELD:
+      return {...state, statuses: {...state.statuses, [field!]: payload}}
+    case RESET_FIELDS:
+      return resetAllFields(state)
+    default:
+      throw Error()
+  }
 }
 
 function getInitialState(options: IOptions): State {
-    return Object.keys(options.fields).reduce<State>(
-        (initialValue, fieldName: string) => {
-            initialValue.values[fieldName] = options.fields[fieldName].value || ''
-            initialValue.statuses[fieldName] = {status: 'standard'}
-            return initialValue
-        },
-        {values: {}, statuses: {}}
-    )
+  return Object.keys(options.fields).reduce<State>(
+    (initialValue, fieldName: string) => {
+      initialValue.values[fieldName] = options.fields[fieldName].value || ''
+      initialValue.statuses[fieldName] = {status: 'standard'}
+      return initialValue
+    },
+    {values: {}, statuses: {}}
+  )
 }
 
 export default function useFormValidation(options: IOptions): IFormValidationResult {
-    const [state, dispatch] = useReducer(reducer, getInitialState(options))
+  const [state, dispatch] = useReducer(reducer, getInitialState(options))
 
-    function validateField(field: string, errorCb?: (message: string) => void, successCb?: () => void): void {
-        try {
-            options.fields[field].rules.forEach(rule => rule(state.values[field]))
-            dispatch({type: VALIDATE_FIELD, field, payload: {status: 'success'}})
-            if (successCb) successCb()
-        } catch (error) {
-            dispatch({
-                type: VALIDATE_FIELD,
-                field,
-                payload: {
-                    status: 'error',
-                    message: error.message
-                }
-            })
-            if (errorCb) errorCb(error.message)
+  function validateField(field: string, errorCb?: (message: string) => void, successCb?: () => void): void {
+
+    if (!state.values[field]) {
+      return dispatch({type: VALIDATE_FIELD, field, payload: {status: 'standard'}})
+    }
+
+    try {
+      options.fields[field].rules.forEach(rule => rule(state.values[field]))
+      dispatch({type: VALIDATE_FIELD, field, payload: {status: 'success'}})
+      if (successCb) successCb()
+    } catch (error) {
+      dispatch({
+        type: VALIDATE_FIELD,
+        field,
+        payload: {
+          status: 'error',
+          message: error.message
         }
+      })
+      if (errorCb) errorCb(error.message)
+    }
+  }
+
+  function thereIsAnError(): boolean {
+    let thereIsAnError = false
+
+    Object.keys(options.fields).forEach((fieldName: string) => {
+      validateField(fieldName, () => {
+        thereIsAnError = true
+      })
+    })
+
+    return thereIsAnError
+  }
+
+  function onChangeField(field: string): (e: React.ChangeEvent<HTMLInputElement>) => void {
+    return function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+      dispatch({type: CHANGE_VALUE, field, payload: e.target.value})
+      validateField(field)
+    }
+  }
+
+  function onSubmit(e: React.SyntheticEvent<HTMLFormElement>): void {
+    if (thereIsAnError()) {
+      e.preventDefault()
+      return
     }
 
-    function thereIsAnError(): boolean {
-        let thereIsAnError = false
+    if (options.form && options.form.onSubmit) options.form.onSubmit(e)
+  }
 
-        Object.keys(options.fields).forEach((fieldName: string) => {
-            validateField(fieldName, () => {
-                thereIsAnError = true
-            })
-        })
+  function resetFields(): void {
+    dispatch({
+      type: RESET_FIELDS
+    })
+  }
 
-        return thereIsAnError
-    }
+  function manuallySetField(fieldName: string, value: string): void {
+    dispatch(
+      {type: CHANGE_VALUE, field: fieldName, payload: value}
+    )
+    validateField(fieldName)
+  }
 
-    function onChangeField(field: string): (e: React.ChangeEvent<HTMLInputElement>) => void {
-        return function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-            dispatch({type: CHANGE_VALUE, field, payload: e.target.value})
-            validateField(field)
+  function mapFieldProps(): InputProps {
+    return Object.keys(options.fields).reduce(
+      function mapStateToInputProps(fieldsObject: InputProps, fieldName: string) {
+        return {
+          ...fieldsObject,
+          [fieldName]: {
+            input: {onChange: onChangeField(fieldName), value: state.values[fieldName]},
+            info: state.statuses[fieldName]
+          }
         }
-    }
+      },
+      {}
+    )
+  }
 
-    function onSubmit(e: React.SyntheticEvent<HTMLFormElement>): void {
-        if (thereIsAnError()) {
-            e.preventDefault()
-            return
-        }
-
-        if (options.form && options.form.onSubmit) options.form.onSubmit(e)
-    }
-
-    return {
-        form: {
-            onSubmit,
-            noValidate: true
-        },
-        fields: Object.keys(options.fields).reduce(
-            (fieldsObject, fieldName: string) => ({
-                ...fieldsObject,
-                [fieldName]: {
-                    input: {onChange: onChangeField(fieldName), value: state.values[fieldName]},
-                    info: state.statuses[fieldName]
-                }
-            }),
-            {}
-        )
-    }
+  return {
+    form: {
+      onSubmit,
+      noValidate: true
+    },
+    fields: mapFieldProps(),
+    resetFields,
+    manuallySetField
+  }
 }
